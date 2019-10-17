c70aec4deb94da3137e2e81ea7de30fc973cf7a1
Felipe Lozano Romero <felipe.lozano1@alu.uclm.es>
2019-10-17 23:13:10 +0200

    Primer commit de la parte del backend

---
+++ spring-backend-restapi/src/main/java/com/spring/backend/restapi/SpringBackendRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.spring.backend.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBackendRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBackendRestapiApplication.class, args);
+	}
+
+}
---
+++ spring-backend-restapi/src/test/java/com/spring/backend/restapi/SpringBackendRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.backend.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBackendRestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
